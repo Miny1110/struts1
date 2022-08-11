72d366e59c62fcb680b9eb5ecc800b78693b8f00
Miny1110 <wjdalswjd453@naver.com>
2022-08-11 16:12:22 +0900

    파일저장~

--- src/com/board/BoardAction.java
+++ src/com/board/BoardAction.java
@@ -8,6 +8,9 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
+import com.util.dao.CommonDAO;
+import com.util.dao.CommonDAOImpl;
+
 public class BoardAction extends DispatchAction {
 //dispatchAction을 상속받으면 if-elseif로 uri에서 주소를 찾아서 실행하던 서블릿 방식과는 달리 그걸 메소드화 해서 사용한다.
 //기존 방식처럼 쓰려면 Action을 상속받으면 된다. (TestAction 파일 참고)
@@ -15,12 +18,32 @@ public class BoardAction extends DispatchAction {
 	public ActionForward created(ActionMapping mapping, ActionForm form, 
 			HttpServletRequest request, HttpServletResponse response) throws Exception {	
 		
-		
-		
-		
-		
 		return mapping.findForward("created");
+	}
+	
+	public ActionForward created_ok(ActionMapping mapping, ActionForm form, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception {	
 		
+		CommonDAO dao = CommonDAOImpl.getInstance();
+		
+		BoardForm f = (BoardForm)form;
+		
+		int maxNum = dao.getIntValue("bbs.maxNum");
+		
+		f.setNum(maxNum+1);
+		f.setIpAddr(request.getRemoteAddr());
+		
+		dao.insertData("bbs.insertData", f);
+		
+		dao = null;
+		
+		return mapping.findForward("created_ok");
+	}
+	
+	public ActionForward list(ActionMapping mapping, ActionForm form, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception {	
+		
+		return mapping.findForward("list");
 	}
 }
 
